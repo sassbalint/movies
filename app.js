04cf1d67057009d1b055018b5476e950773f879d
Sass Bálint <sass.balint@nytud.hu>
2023-02-17 17:02:39 +0100

    [mod] handle all data fields -- deployed :tada:

--- app.js
+++ app.js
@@ -19,19 +19,32 @@ d3.csv("dictbibl.csv").then(function (data) {
     noresults = d3.select("p").classed('noresults', true)
 
     if (inputValue.length < 3){
-      noresults.html("Légyszi minimum 3 karaktert.")
+      noresults.html("Minimum 3 karaktert adjon meg.")
       return 0;
     }
 
     // ezt hogy lehet szebben? XXX
     var filteredData = movies.filter(
       movie =>
-      movie.isbn.toLowerCase().trim().includes(inputValue) ||
+      movie.id.toLowerCase().trim().includes(inputValue) ||
+      movie.id2.toLowerCase().trim().includes(inputValue) ||
+      movie.group.toLowerCase().trim().includes(inputValue) ||
+      movie.pubauthor.toLowerCase().trim().includes(inputValue) ||
+      movie.pubeditor.toLowerCase().trim().includes(inputValue) ||
+      movie.pubeditor_type.toLowerCase().trim().includes(inputValue) ||
+      movie.pubdate.toLowerCase().trim().includes(inputValue) ||
       movie.pubtitle.toLowerCase().trim().includes(inputValue) ||
+      movie.volume.toLowerCase().trim().includes(inputValue) ||
       movie.pubshort.toLowerCase().trim().includes(inputValue) ||
+      movie.staff.toLowerCase().trim().includes(inputValue) ||
       movie.publisher.toLowerCase().trim().includes(inputValue) ||
       movie.pubplace.toLowerCase().trim().includes(inputValue) ||
-      movie.pubdate.toLowerCase().trim().includes(inputValue)
+      movie.series.toLowerCase().trim().includes(inputValue) ||
+      movie.pp.toLowerCase().trim().includes(inputValue) ||
+      movie.size.toLowerCase().trim().includes(inputValue) ||
+      movie.isbn.toLowerCase().trim().includes(inputValue) ||
+      movie.issn.toLowerCase().trim().includes(inputValue) ||
+      movie.remark.toLowerCase().trim().includes(inputValue)
     );
     length = filteredData.length
 
@@ -46,13 +59,25 @@ d3.csv("dictbibl.csv").then(function (data) {
 
     for (var i = 0; i < length; i++) {
       d3.select("tbody").insert("tr").html(
-        "<td>" + [i+1] + "</td>" +
-        "<td>" + (output[i]['isbn']) + "</td>" +
+        "<td>" + (output[i]['id']) + "</td>" +
+        "<td>" + (output[i]['id2']) + "</td>" +
+        "<td>" + (output[i]['group']) + "</td>" +
+        "<td>" + (output[i]['pubauthor']) + "</td>" +
+        "<td>" + (output[i]['pubeditor']) + "</td>" +
+        "<td>" + (output[i]['pubeditor_type']) + "</td>" +
+        "<td>" + (output[i]['pubdate']) + "</td>" +
         "<td>" + (output[i]['pubtitle']) + "</td>" +
+        "<td>" + (output[i]['volume']) + "</td>" +
         "<td>" + (output[i]['pubshort']) + "</td>" +
+        "<td>" + (output[i]['staff']) + "</td>" +
         "<td>" + (output[i]['publisher']) + "</td>" +
         "<td>" + (output[i]['pubplace']) + "</td>" +
-        "<td>" + (output[i]['pubdate']) + "</td>"
+        "<td>" + (output[i]['series']) + "</td>" +
+        "<td>" + (output[i]['pp']) + "</td>" +
+        "<td>" + (output[i]['size']) + "</td>" +
+        "<td>" + (output[i]['isbn']) + "</td>" +
+        "<td>" + (output[i]['issn']) + "</td>" +
+        "<td>" + (output[i]['remark']) + "</td>"
       )
     }
   };
--- data/xml2csv.py
+++ data/xml2csv.py
@@ -19,54 +19,57 @@ def main():
     parser = ET.XMLParser(remove_blank_text=True)
     doc = ET.parse(FILENAME, parser)
 
+    ATTR, ELEM, LIST = 0, 1, 2
+
     FIELDNAMES = [
-        'isbn', 'pubtitle', 'pubshort', 'publisher', 'pubplace', 'pubdate'
+        'id', 'id2', 'group',
+        'pubauthor',
+        'pubeditor', 'pubeditor_type',
+        'pubdate', 'pubtitle', 'volume', 'pubshort', 'staff',
+        'publisher', 'pubplace', 'series', 'pp', 'size',
+        'isbn', 'issn', 'remark'
+    ]
+
+    # xpath can be different from field name
+    XPATHS = [
+        '@id', '@id2', '@group',
+        'pubauthor_grp/pubauthor',
+        'pubeditor_grp/pubeditor', 'pubeditor_grp/@type'
+    ] + FIELDNAMES[6:]
+
+    TYPES = [
+        ATTR, ATTR, ATTR,
+        LIST,
+        LIST, ATTR,
+        ELEM, ELEM, ELEM, ELEM, ELEM,
+        ELEM, ELEM, ELEM, ELEM, ELEM,
+        ELEM, ELEM, ELEM
     ]
 
     print(','.join(FIELDNAMES))
 
     for entry in doc.xpath("//source"):
-        fieldvalues = []
-        for fieldname in FIELDNAMES:
-            fieldval = entry.xpath(fieldname)
-            if fieldval:
-                text = ''.join(fieldval[0].itertext()).replace('\n', ' ') # [0] !!!
+        row = []
+        for xpath, typ in zip(XPATHS, TYPES):
+            fieldvalue = entry.xpath(xpath)
+            if fieldvalue:
+                if typ == ATTR:
+                    text = fieldvalue[0]
+                # ok for ELEM too as it is a 1-element LIST always
+                elif typ in [ELEM, LIST]:
+                    text = ' & '.join(
+                        '/'.join(val.itertext()).replace('\n', ' ')
+                        for val in fieldvalue)
+                # '/' if for handling <print> -- could be better XXX
+                else:
+                    exit(f'!!! Unknown type: {typ}')
                 value = f'"{text}"'
             else:
-                value = ''
-            fieldvalues.append(value)
-        print(','.join(fieldvalues))
+                value = '–'
+            row.append(value)
+        print(','.join(row))
 
 
 if __name__ == '__main__':
     main()
-
-
-# database
-#  sourcelist ( source+ ) >
-# 
-# record
-#  source ( (pubauthor_grp | pubeditor_grp), pubdate, pubtitle, volume?, pubshort?, staff?, publisher, pubplace, series?, (pp | size | isbn | issn)*, remark* ) >
-# 
-# fields -- minden lehet ures!
-#  source/@id
-#  source/@id2
-#  source/@group
-#  pubeditor_grp ( pubeditor+ ) ***LIST
-#  pubeditor_grp/@type
-#  pubauthor_grp ( pubauthor+ ) ***LIST
-#  pubshort
-#  isbn
-#  remark
-#  staff
-#  pubtitle
-#  volume
-#  print
-#  pp
-#  size
-#  issn
-#  series
-#  publisher
-#  pubplace
-#  pubdate
 
